2da3884bf1d5abed66e435653e4ecd82a8ad8590
EAZY BLACK <68810731+EAZYBLACK@users.noreply.github.com>
2022-02-19 16:34:09 +0000

    erg

--- editor.js
+++ editor.js
@@ -1,6 +1,6 @@
-editor = document.getElementById("editor");
+var editor = document.getElementById("editor");
 function editor(param1, param2, param3) {
   if (param1 == "load" || param1 == 'load' || param1 == load) {
-    editor.
+    editor.defaultValue = "$nomention\n";
   }
 }
